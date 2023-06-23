ad3ff8f9be0bcd7387b30320958b4deb252b47e5
mgregerson <mgregerson4321@gmail.com>
2023-06-23 14:25:21 -0400

    Refactored relationships between Users and reservations. Added start_date and end_date to reservations, added Message Model, added Conversation Model, added new seed data. Introduced new routes for getting all conversations, and posting a new reservation

--- models.py
+++ models.py
@@ -49,6 +49,10 @@ class User(db.Model):
 
     reservations = db.relationship('Reservation', backref='users')
 
+    user_sent_messages = db.relationship('Message', backref='sender_user', foreign_keys='Message.sender_username')
+
+    user_received_messages = db.relationship('Message', backref='recipient_user', foreign_keys='Message.recipient_username')
+
     def serialize(self):
         """Serialize to dictionary."""
 
@@ -171,9 +175,6 @@ class Rating(db.Model):
 
     __tablename__ = 'ratings'
 
-    def __repr__(self):
-        return f"<Rating #{self.id}: {self.rental_id} rating: {self.rating}>"
-
     id = db.Column(
         db.Integer,
         primary_key=True
@@ -190,6 +191,8 @@ class Rating(db.Model):
         nullable=False,
     )
 
+    rental = db.relationship('Rental', backref='rating')
+
     def serialize(self):
         """Serialize to dictionary."""
 
@@ -199,8 +202,20 @@ class Rating(db.Model):
             "rental_id": self.rental_id
         }
 
+    @classmethod
+    def add_rating(cls, rating, rental_id):
+        """Class method to add a rating to the database"""
+
+        rating = Rating(
+            rating=rating,
+            rental_id=rental_id
+        )
+
+        db.session.add(rating)
+        return rating
+
 class Reservation(db.Model):
-    """ Reservation on each rental """
+    """Reservation on each rental"""
 
     __tablename__ = 'reservations'
 
@@ -209,7 +224,12 @@ class Reservation(db.Model):
         primary_key=True
     )
 
-    date = db.Column(
+    start_date = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    end_date = db.Column(
         db.String(50),
         nullable=False
     )
@@ -232,11 +252,12 @@ class Reservation(db.Model):
     )
 
     @classmethod
-    def add_reservation(cls, date, rental_id, renter, rating=None):
-        """Class method to add a rental to the database"""
+    def add_reservation(cls, start_date, end_date, rental_id, renter, rating=None):
+        """Class method to add a reservation to the database"""
 
         reservation = Reservation(
-            date=date,
+            start_date=start_date,
+            end_date=end_date,
             rental_id=rental_id,
             renter=renter,
             rating=rating,
@@ -246,17 +267,136 @@ class Reservation(db.Model):
         return reservation
 
     def serialize(self):
-        """Serialize to dictionary."""
+        """Serialize to dictionary"""
 
         return {
             "id": self.id,
-            "date": self.date,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
             "rating": self.rating,
             "rental_id": self.rental_id,
             "renter": self.renter
         }
 
 
+class Message(db.Model):
+    """ Messages table """
+
+    __tablename__ = 'messages'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    content = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    timestamp = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow
+    )
+
+    conversation_id = db.Column(
+        db.Integer,
+        db.ForeignKey('conversations.id', ondelete='CASCADE'),
+        nullable=False
+    )
+
+    conversation = db.relationship('Conversation', backref='messages')
+
+    sender_username = db.Column(
+        db.Text,
+        db.ForeignKey('users.username', ondelete='CASCADE'),
+        nullable=False
+    )
+
+    sender = db.relationship('User', backref='sent_messages', foreign_keys=[sender_username])
+
+    recipient_username = db.Column(
+        db.Text,
+        db.ForeignKey('users.username', ondelete='CASCADE'),
+        nullable=False
+    )
+
+    recipient = db.relationship('User', backref='received_messages', foreign_keys=[recipient_username])
+
+    @classmethod
+    def create_message(cls, content, sender_username, recipient_username, conversation_id):
+        """Create a new message and add it to the database."""
+
+        message = Message(
+            content=content,
+            sender_username=sender_username,
+            recipient_username=recipient_username,
+            conversation_id=conversation_id
+        )
+
+        db.session.add(message)
+        return message
+
+    def serialize(self):
+        """Serialize to dictionary."""
+
+        return {
+            "id": self.id,
+            "content": self.content,
+            "timestamp": self.timestamp.isoformat(),
+            "conversation_id": self.conversation_id,
+            "sender_username": self.sender_username,
+            "recipient_username": self.recipient_username
+        }
+
+    
+class Conversation(db.Model):
+    """ Conversations between users """
+
+    __tablename__ = 'conversations'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    user1_username = db.Column(
+        db.Text,
+        db.ForeignKey('users.username', ondelete='CASCADE'),
+        nullable=False
+    )
+
+    user1 = db.relationship('User', foreign_keys=[user1_username])
+
+    user2_username = db.Column(
+        db.Text,
+        db.ForeignKey('users.username', ondelete='CASCADE'),
+        nullable=False
+    )
+
+    user2 = db.relationship('User', foreign_keys=[user2_username])
+
+    @classmethod
+    def create_conversation(cls, user1_username, user2_username):
+        """Create a new conversation between two users and add it to the database."""
+
+        conversation = Conversation(
+            user1_username=user1_username,
+            user2_username=user2_username
+        )
+
+        db.session.add(conversation)
+        return conversation
+
+    def serialize(self):
+        """Serialize to dictionary."""
+
+        return {
+            "id": self.id,
+            "user1_username": self.user1_username,
+            "user2_username": self.user2_username
+        }
 
 def connect_db(app):
     """Connect this database to provided Flask app.
